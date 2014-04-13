9947e6f9b225e3b15c807cc055a72c049b6be49c
Ubuntu <ubuntu@ip-172-31-1-181.eu-west-1.compute.internal>
2014-04-13 08:04:17 +0000

    Fixed web.js with old version

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
 var fs = require('fs');
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
-var output = new Buffer(1000);
-output = fs.readFileSync('index.html');
-response.send(output.toString());
+var html = fs.readFileSync(htmlfile).toString();
+// var output = new Buffer(1000);
+// output = fs.readFileSync('index.html');
+response.send(html);
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
